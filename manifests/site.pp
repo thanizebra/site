c1fe5f85d97d4cba77e0ab7abc72af484168ff4a
Thaniyarasu <dvh638@zebra.com>
2015-01-31 13:50:43 +0000

    Apache2 added

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,3 +2,15 @@ Exec {
   path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
 }
 
+package {
+  "apache2":
+    ensure => present
+}
+
+service {
+  "apache2":
+    ensure => true,
+    enable => true
+}
+
+
